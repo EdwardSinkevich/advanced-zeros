9dd37c111ad5f3615e2908fee9d826107e9924c8
Edward <mejik-yo@mail.ru>
2019-03-08 01:34:31 +0300

    feat: implement getZerosCount function, which calculate zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let primeNum = 2;
+  let primeCount;
+  let countersArr = [];
+  let zeroCount = 0;
+  for (; primeNum <= base; primeNum++) {
+    if (base % primeNum === 0) {
+      primeCount = 0;
+      zeroCount = 0;
+      while (base % primeNum === 0) {
+        base = base / primeNum;
+        primeCount++;
+      }
+      let multiplier = primeNum;
+      while (multiplier < number) {
+        zeroCount = zeroCount + Math.floor(number / multiplier);
+        multiplier = multiplier * primeNum;
+      }
+      countersArr.push(Math.floor(zeroCount/primeCount));
+    }  
+  }
+  return (Math.min.apply(Math, countersArr));
 }
